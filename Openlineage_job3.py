97aa358469f089a6d52479fdad7df45f10e98492
jitsopin <57101770+jitsopin@users.noreply.github.com>
2025-10-18 16:45:11 +0700

    commit

--- Openlineage_job3.py
+++ Openlineage_job3.py
@@ -2,71 +2,49 @@ import requests
 import json
 
 # Define the payload for the new job
+# Define the payload for the new job
 payload = {
     "eventType": "COMPLETE",
-    "eventTime": "2025-10-18T09:10:13.770024",
+    "eventTime": "2025-10-18T09:10:13.770024+10:00",
     "run": {
         "runId": "37eb6006-6780-4abe-bf2d-52007f711943"
     },
     "job": {
         "namespace": "Apache Spark / File System",
-        "name": "overwrite"
+        "name": "new-job"
     },
-    "inputs": [
-        {
-            "namespace": "Apache Spark / File System",
-            "name": "parquet.employees",
-            "facets": {
-                "schema": {
-                    "_producer": "https://github.com/OpenLineage/OpenLineage/blob/v1-0-0/client",
-                    "_schemaURL": "https://github.com/OpenLineage/OpenLineage/blob/v1-0-0/spec/OpenLineage.json#/definitions/SchemaDatasetFacet",
-                    "fields": [
-                        {
-                            "name": "id",
-                            "type": "INTEGER"
-                        },
-                        {
-                            "name": "name",
-                            "type": "STRING"
-                        },
-                        {
-                            "name": "steps_to_desk",
-                            "type": "FLOAT"
-                        }
-                    ]
-                }
+    "inputs": [{
+        "namespace": "new-namespace",
+        "name": "new-input-dataset",
+        "facets": {
+            "schema": {
+                "_producer": "https://github.com/OpenLineage/OpenLineage/blob/v1-0-0/client",
+                "_schemaURL": "https://github.com/OpenLineage/OpenLineage/blob/v1-0-0/spec/OpenLineage.json#/definitions/SchemaDatasetFacet",
+                "fields": [
+                    {"name": "input_col1", "type": "INTEGER"},
+                    {"name": "input_col2", "type": "STRING"}
+                ]
             }
         }
-    ],
-    "outputs": [
-        {
-            "namespace": "Apache Spark / File System",
-            "name": "csv.loaded_data",
-            "facets": {
-                "schema": {
-                    "_producer": "https://github.com/OpenLineage/OpenLineage/blob/v1-0-0/client",
-                    "_schemaURL": "https://github.com/OpenLineage/OpenLineage/blob/v1-0-0/spec/OpenLineage.json#/definitions/SchemaDatasetFacet",
-                    "fields": [
-                        {
-                            "name": "id",
-                            "type": "INTEGER"
-                        },
-                        {
-                            "name": "name",
-                            "type": "STRING"
-                        },
-                        {
-                            "name": "steps_to_desk",
-                            "type": "FLOAT"
-                        }
-                    ]
-                }
+    }],
+    "outputs": [{
+        "namespace": "new-namespace",
+        "name": "new-output-dataset",
+        "facets": {
+            "schema": {
+                "_producer": "https://github.com/OpenLineage/OpenLineage/blob/v1-0-0/client",
+                "_schemaURL": "https://github.com/OpenLineage/OpenLineage/blob/v1-0-0/spec/OpenLineage.json#/definitions/SchemaDatasetFacet",
+                "fields": [
+                    {"name": "output_col1", "type": "FLOAT"},
+                    {"name": "output_col2", "type": "BOOLEAN"}
+                ]
             }
         }
-    ],
+    }],
     "producer": "https://github.com/OpenLineage/OpenLineage/blob/v1-0-0/client",
     "schemaURL": "https://openlineage.io/spec/1-0-5/OpenLineage.json#/definitions/RunEvent"
 }
+
 
 # Define the API endpoint
 url = "http://localhost:5000/api/v1/lineage"
